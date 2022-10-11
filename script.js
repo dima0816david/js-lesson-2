840ac4ff64b7cbf9cfdc16736e618d409ecbbd8f
Dilshodbek <dilshodtuit217@gmail.com>
2022-10-12 03:26:34 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+// console.log('1-masala:');
+// let l = 496;
+// let m = Math.floor(l / 100);
+// console.log(m +' metr');
+
+// ----------------------------->
+
+// console.log('2-masala:');
+// let k = 7281;
+// let t = Math.floor(k / 1000);
+// console.log(t + ' tonna');
+
+// ------------------------------>
+
+// console.log('3-masala:');
+// let a = 147;
+// let b = 32;
+// let k = Math.floor(a / b);
+// console.log(k + ' marta');
+
+// ------------------------------>
+
+// console.log('4-masala:');
+// let s = 173;
+// let o = Math.trunc(s / 10);
+// let b = s % 10;
+// console.log(o);
+// console.log(b);
+
+// ------------------------------>
+
+// console.log('5-masala:');
+// let s = 97;
+// let a = Math.trunc(s / 10);
+// let b = s % 10;
+// console.log(a + b);
+
+// ------------------------------>
+
+// console.log('6-masala:');
+// let s = 84;
+// let a = Math.trunc(s / 10);
+// let b = s % 10;
+// console.log(a * b);
+
+// ------------------------------>
+
+// console.log('7-masala:');
+// let k = 1847;
+// let y = Math.floor(k / 100);
+// let z = Math.floor(y % 100);
+// console.log(z +' yuzlik bor');
+
+// ------------------------------>
+
+// console.log('8-masala:');
+// let s = 12437;
+// let m = Math.floor(s / 60);
+// let l = Math.floor(s / 3600);
+// console.log(m +' minut');
+// console.log(l +' soat');
